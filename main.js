356c64ff2821de5e148ddb1a01a304abcf6a022f
m34wzaa <daaaavethemagicalcheesewizard@gmail.com>
2026-04-28 14:22:22 +0000

    ouiu

--- main.js
+++ main.js
@@ -6,20 +6,44 @@ function vect(x, y, z) {
 }
 function quat(rot, vectr) {
     const rotation = rot / 180 * Math.PI;
-    const output = new THREE.Quaternion().setFromAxisAngle(vectr, rotation);
+    const vector = vectr == 'x' ? vect(1, 0, 0) : vectr == 'y' ? vect(0, 0, 1) : vectr == 'z' ? vect(0, 0, 1) : null;
+    const output = new THREE.Quaternion().setFromAxisAngle(vector, rotation);
     return output;
 }
-function rail(vec1, quat1, vec2, quat2) {
-    //empty
+let alignment =  [[vect(0, 0, 0)]];
+let rightRails = [[vect(0.5, 0, 0)]];
+let leftRails =  [[vect(-0.5, 0, 0)]];
+function rail(target, coaster) {
+    //make rail from point a to point b
+    alignment[coaster].push(target)
+    const align = new THREE.TubeGeometry(alignment)
 }
-const scene = new THREE.Scene()
+let isEditor = false;
+function editor() {
+    if (isEditor) {
+
+    }
+}
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x222222);
+const fog = new THREE.FogExp2(0x222222, 0.03);
+scene.add(fog);
 const render = new THREE.WebGLRenderer();
 let fov = 90;
-let renderDistance = 1000
+let renderDistance = 1000;
 const cam = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.01, renderDistance);
+let keys = [];
+document.addEventListener('keydown', (e) => {
+    keys[e.key] = true;
+});
+document.addEventListener('keyup', (e) => {
+    keys[e.key] = false;
+});
+
+// adding the framework for rails
 function anim() {
     requestAnimationFrame(anim);
     //anim loop
-    render.render(scene, cam)
+    render.render(scene, cam);
 }
-anim()
+anim();
